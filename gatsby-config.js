e97d16006850ef5e10f562ab6b654ea4b22a14d1
SimonTrolldenier <66159033+SimonTrolldenier@users.noreply.github.com>
2020-05-30 11:40:58 +0200

    Change Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Simon's Electronics Workbench`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`
